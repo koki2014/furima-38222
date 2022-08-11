2d1cb7b82bc58af97f979ca89b72f1513ed5464f
koki2014 <susandayo1031@icloud.com>
2022-08-12 02:47:57 +0900

    テストコード追加・販売手数料、利益の表示

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
+    name                 {'あああ'}
+    text                 {'あああ'}
+    category_id          {'2'}
+    condition_id         {'2'}
+    charge_bearer_id     {'2'}
+    shipping_area_id     {'2'}
+    delivary_day_id      {'2'}
+    price                {'300'}
     
   end
 end
