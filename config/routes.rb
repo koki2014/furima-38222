57d3c497758e05814818eefe9a4361f641f9de44
koki2014 <susandayo1031@icloud.com>
2022-08-19 18:19:17 +0900

    ルーティング　修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   
-  resources :items do
-    resources :orders, only: [:index, :create]
+  
   end
 end
 
